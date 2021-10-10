040104a1f823c82ac995109fa13ce648f8f455a9
shinhyogeun <tls1gy2rms3@naver.com>
2021-10-10 23:13:08 +0900

    Next 설치

---
+++ front/pages/profile.js
@@ -0,0 +1,7 @@
+const Profile = () => {
+    return (
+        <div>피로필 페이지</div>
+    );
+}
+
+export default Profile;
---
+++ front/pages/signup.js
@@ -0,0 +1,7 @@
+const Signup = () => {
+    return (
+        <div>회원가입페이지</div>
+    );
+}
+
+export default Signup;
